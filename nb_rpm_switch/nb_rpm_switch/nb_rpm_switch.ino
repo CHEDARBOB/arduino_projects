8f3f198ffc56743062e0adafa76b9714253d48b0
Trevor <trevor.overby4@gmail.com>
2020-02-20 07:40:56 -0700

    finished first version. TO-DO test

--- nb_rpm_switch/nb_rpm_switch/nb_rpm_switch.ino
+++ nb_rpm_switch/nb_rpm_switch/nb_rpm_switch.ino
@@ -1,39 +1,55 @@
 bool edge_a = false;
 const int rpm_pin = 3;
-const int vics_signal_out = 4;
+const int vics_signal = 4;
+const int vics_LED = LED_BUILTIN;
+const int desired_rpm = 5200;
 int rpm = 0;
-unsigned long _time = 0;
 unsigned long period = 0;
 unsigned long falling_edge_a = 0;
 unsigned long falling_edge_b = 0;
 
 unsigned long calc_rpm(unsigned long edge_a, unsigned long edge_b){
+  //period of 11.5 ms == 5217 rpm
   unsigned long _period = edge_b - edge_a;
-  int _rpm = (60*1000)/period;
+  int _rpm = 60000/period;
   return rpm
 }
 
-open_valve(int _rpm){
-  
+void open_valve(int _rpm){
+  if(_rpm > desired_rpm){
+    digitalWrite(vics_signal, HIGH)
+    digitalWrite(vics_LED, HIGH)
+  }
+  else{
+    digitalWrite(vics_signal, LOW)
+    digitalWrite(vics_LED, LOW)
+  }
 }
 void setup() {
   pinMode(rpm_pin, INPUT);
-  pinMode(vics_signal_out, OUTPUT);
+  pinMode(vics_signal, OUTPUT);
+  pinMode(vics_LED, OUTPUT);
+  digitalWrite(vics_LED, LOW);
 }
 
 void loop() {
   //take the period starting from falling edge to falling edge. 
-  //a certain period should represent a curtain RPM. 
-  if (rpm_pin == HIGH && edge_a == false){
-   //assume valid edge, set edge_a
-   falling_edge_a = millis();
-   edge_a = true;
-   continue;
+  //a certain period should represent a curtain RPM.
+  //assume valid edge, set edge_a 
+  if(rpm_pin == HIGH){
+    if(edge_a == false){
+       falling_edge_a = millis();
+       edge_a = true;
+    }
+    else{
+      falling_edge_b = millis()
+      edge_a = false;
+    }
   }
-  else if(rpm_pin == HIGH && edge_a == true){
-   falling_edge_b = millis()
-   edge_a = false;
-   rpm = calc_rpm(falling_edge_a, falling_edge_b);
-   open_valve(rpm);
+  //pin is low AND both edges are valid. 
+  //Check rpm and open valve if desired rpm is reached.
+  if(rpm_pin == LOW && (falling_edge_a < falling_edge_b)){
+    rpm = calc_rpm(falling_edge_a, falling_edge_b);
+    open_valve(rpm);
   }
 }
