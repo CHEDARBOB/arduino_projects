5680af8bbf56803d13e082ed31f1d6418918c261
Trevor <trevor.overby4@gmail.com>
2020-02-20 08:32:18 -0700

    finished first.1 version. TO-DO test

--- nb_rpm_switch/nb_rpm_switch/nb_rpm_switch.ino
+++ nb_rpm_switch/nb_rpm_switch/nb_rpm_switch.ino
@@ -4,13 +4,14 @@ const int vics_signal = 4;
 const int vics_LED = LED_BUILTIN;
 const int desired_rpm = 5200;
 int rpm = 0;
+int rpm_signal = 0;
 unsigned long period = 0;
 unsigned long falling_edge_a = 0;
 unsigned long falling_edge_b = 0;
 
-unsigned long calc_rpm(unsigned long edge_a, unsigned long edge_b){
+unsigned long calc_rpm(unsigned long _edge_a, unsigned long _edge_b){
   //period of 11.5 ms == 5217 rpm
-  unsigned long _period = edge_b - edge_a;
+  unsigned long _period = _edge_b - _edge_a;
   int _rpm = 60000/period;
   return rpm
 }
@@ -35,8 +36,9 @@ void setup() {
 void loop() {
   //take the period starting from falling edge to falling edge. 
   //a certain period should represent a curtain RPM.
-  //assume valid edge, set edge_a 
-  if(rpm_pin == HIGH){
+  //assume valid edge, set edge_a
+  rpm_signal = digitalRead(rpm_pin) 
+  if(rpm_signal == HIGH){
     if(edge_a == false){
        falling_edge_a = millis();
        edge_a = true;
@@ -48,7 +50,7 @@ void loop() {
   }
   //pin is low AND both edges are valid. 
   //Check rpm and open valve if desired rpm is reached.
-  if(rpm_pin == LOW && (falling_edge_a < falling_edge_b)){
+  if(rpm_signal == LOW && (falling_edge_a < falling_edge_b)){
     rpm = calc_rpm(falling_edge_a, falling_edge_b);
     open_valve(rpm);
   }
