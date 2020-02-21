407af569afd113e8c4a899b61fb18c58f3b39168
CHEDARBOB <trevor.overby4@gmail.com>
2020-02-20 19:55:58 -0700

    Tested with delay().

--- nb_rpm_switch/nb_rpm_switch.ino
+++ nb_rpm_switch/nb_rpm_switch.ino
@@ -4,26 +4,27 @@ const int vics_signal = 4;
 const int vics_LED = LED_BUILTIN;
 const int desired_rpm = 5200;
 int rpm = 0;
-int rpm_signal = 0;
+int rpm_signal = HIGH;
 unsigned long period = 0;
 unsigned long falling_edge_a = 0;
 unsigned long falling_edge_b = 0;
 
 unsigned long calc_rpm(unsigned long _edge_a, unsigned long _edge_b){
-  //period of 11.5 ms == 5217 rpm
-  unsigned long _period = _edge_b - _edge_a;
-  int _rpm = 60000/period;
-  return rpm
+  //period of 5.979 ms = ~5000 rpm
+  float _period = _edge_b - _edge_a;
+  int _rpm = ((60000*1000)/(_period*2));
+  return rpm;
 }
 
 void open_valve(int _rpm){
   if(_rpm > desired_rpm){
-    digitalWrite(vics_signal, HIGH)
-    digitalWrite(vics_LED, HIGH)
+    digitalWrite(vics_signal, HIGH);
+    digitalWrite(vics_LED, HIGH);
+    
   }
   else{
-    digitalWrite(vics_signal, LOW)
-    digitalWrite(vics_LED, LOW)
+    digitalWrite(vics_signal, LOW);
+    digitalWrite(vics_LED, LOW);
   }
 }
 void setup() {
@@ -37,18 +38,18 @@ void loop() {
   //take the period starting from falling edge to falling edge. 
   //a certain period should represent a curtain RPM.
   //assume valid edge, set edge_a
-  rpm_signal = digitalRead(rpm_pin) 
+  rpm_signal = digitalRead(rpm_pin); 
   if(rpm_signal == HIGH){
     if(edge_a == false){
-       falling_edge_a = millis();
+       falling_edge_a = micros();
        edge_a = true;
     }
     else{
-      falling_edge_b = millis()
+      falling_edge_b = micros();
       edge_a = false;
     }
   }
-  //pin is low AND both edges are valid. 
+  //pin is low AND both edges are valid. This should account for overflow as well.
   //Check rpm and open valve if desired rpm is reached.
   if(rpm_signal == LOW && (falling_edge_a < falling_edge_b)){
     rpm = calc_rpm(falling_edge_a, falling_edge_b);
