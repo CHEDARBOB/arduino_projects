24c596e7108d4843f0279e3b480595349e6bd493
Trevor Overby <mr.blarg@gmail.com>
2020-03-03 18:11:40 -0700

    Tuned data & start condition

--- narrowband_sim/narrowband_sim.ino
+++ narrowband_sim/narrowband_sim.ino
@@ -8,11 +8,11 @@ int max_narrowband_voltage = 1000; //mV
 */
 
 int analog_pin = A0;
-int analog_out = 6;
+int analog_out = 9;
 int ox_val = 0;
 //Wideband magic numbers
-int rich = 583;
-int lean = 646;
+int rich = 650;
+int lean = 670;
 int stoich = 614;
 //Narrow max AFR
 int rich_max = 358; 
@@ -20,31 +20,44 @@ int lean_max = 920;
 int range = lean_max - rich_max;
 
 void set_narrow_signal(int val){
-  Serial.println(val);
   if(val < rich){
-    analogWrite(analog_out, 42); //~850mV duty cycle
+    analogWrite(analog_out, 200); //~850mV duty cycle
+    //Serial.print("rich ");
+    //Serial.println(val);
   }
   else if(val > lean){
-    analogWrite(analog_out, 9); //~175 mV
+    //Serial.print("Lean ");
+    //Serial.println(val);
+    analogWrite(analog_out, 0); //~175 mV
   }
   else{
-    analogWrite(analog_out, 25); 
+    analogWrite(analog_out, 100);
   }
+  
 }
 void set_variable_narrow_signal(int val){
   
 }
 void setup() {
-  Serial.begin(9600);
+  //delay(15000);
+  //Serial.begin(9600);
+  analogReference(DEFAULT);
   pinMode(analog_out, OUTPUT);
 }
 /*
  * narrowband sim
  */
 void loop() {
+  //delay(100);
   //583 ~= 14.07 rich condition
   //614 ~= 14.7 stoich
   //646 ~= 14.98 lean condition
+  
   ox_val = analogRead(analog_pin);
-  set_narrow_signal(ox_val);
+  if(ox_val > 350){
+    set_narrow_signal(ox_val);
+  }
+  else{
+    analogWrite(analog_out, 100);
+  }
 }
