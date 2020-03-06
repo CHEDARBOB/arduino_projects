a704f63784e857fb9a33aec28efb2c3e7b8518f6
Trevor <trevor.overby4@gmail.com>
2020-03-06 08:14:08 -0700

    start mag narrow sim.

--- narrowband_sim/narrowband_sim.ino
+++ narrowband_sim/narrowband_sim.ino
@@ -10,50 +10,33 @@ int max_narrowband_voltage = 1000; //mV
 int analog_pin = A0;
 int analog_out = 9;
 int ox_val = 0;
+int offset_ox_val = 0;
 int temp = 0;
+int o2_signal = 0;
 //Wideband magic numbers
 int rich = 630; //AFR ~14.6
 int rich_2 = 576; //14
 int lean = 644; //AFR ~14.8
 int lean_2 = 662; //15
 int stoich = 637; //14.7
-//Narrow max AFR
-int rich_max = 490; //AFR ~13
-int lean_max = 749; //AFR ~16
-int range = lean_max - rich_max;
-//0-1 (0 to 100% duty cycle.)
+int afr_range = 100;
 
+//if lean offset = 1. Rich, offset = 0.
+int set_magnitude(int offset, int afr){
+  temp = abs(stoich - afr);
+  magnitude = offset - (afr_range / temp);
+  return magnitude
+}
 void set_narrow_signal(int val){
-  //mixture is rich
-  if(val < rich){
-    Serial.print("rich ");
-    //Serial.println(val);
-    temp = 135;
-    if(val < rich_2){
-      temp = 170;
-    }
-    else if(val < rich_max){
-      temp = 220;
-    }
-    analogWrite(analog_out, temp);
+  //rich
+  if(val < soich){
+    o2_signal = 255*set_magnitude(0, val)
   }
-  //mixture is lean
-  else if(val > lean){
-    Serial.print("Lean ");
-    //Serial.println(val);
-    temp = 60;
-    if(val > lean_2){
-      temp = 30;
-    }
-    else if(val < lean_max){
-      temp = 0;
-    }
-    analogWrite(analog_out, temp);
-  }
+  //lean
   else{
-    Serial.print("Stioch ");   
-    analogWrite(analog_out, 100);
+    o2_signal = 255*set_magnitude(1, val)
   }
+  Serial.print(o2_signal;)
 }
 void setup() {
   Serial.begin(9600);
@@ -69,10 +52,15 @@ void loop() {
   //646 ~= 14.98 lean condition
   Serial.println(ox_val);
   ox_val = analogRead(analog_pin);
-  if(ox_val > 350){
-    set_narrow_signal(ox_val);
+  if(ox_val < (stoich - afr_range)){
+    offset_ox_val = stoich - 100;
+    set_narrow_signal(offset_ox_val);
+  }
+  else if(ox_val > (stoich + afr_range)){
+    offset_ox_val = stoich + 100;
+    set_narrow_signal(offset_ox_val);
   }
   else{
-    analogWrite(analog_out, 100);
+    set_narrow_signal(ox_val);
   }
 }
