d231f5ce70988b0ce3da9745fe6aaf926482ed21
Trevor Overby <mr.blarg@gmail.com>
2020-03-06 17:09:06 -0700

    tuning.

--- narrowband_sim/narrowband_sim.ino
+++ narrowband_sim/narrowband_sim.ino
@@ -8,15 +8,15 @@ int max_narrowband_voltage = 1000; //mV
 */
 
 int analog_pin = A0;
-int analog_out = 6;
+int analog_out = 9;
 int ox_val = 0;
 int temp = 0;
 //Wideband magic numbers
-int rich = 630; //AFR ~14.6
-int rich_2 = 576; //14
-int lean = 644; //AFR ~14.8
-int lean_2 = 662; //15
-int stoich = 637; //14.7
+int rich = 639; //AFR ~14.6
+int rich_2 = 593; //14
+int lean = 646; //AFR ~14.8
+int lean_2 = 660; //15
+int stoich = 644; //14.7
 //Narrow max AFR
 int rich_max = 490; //AFR ~13
 int lean_max = 749; //AFR ~16
@@ -28,12 +28,12 @@ void set_narrow_signal(int val){
   if(val < rich){
     Serial.print("rich ");
     //Serial.println(val);
-    temp = 135;
+    temp = 140;
     if(val < rich_2){
-      temp = 170;
+      temp = 190;
     }
     else if(val < rich_max){
-      temp = 200;
+      temp = 225;
     }
     analogWrite(analog_out, temp);
   }
@@ -41,22 +41,23 @@ void set_narrow_signal(int val){
   else if(val > lean){
     Serial.print("Lean ");
     //Serial.println(val);
-    temp = 60;
+    temp = 40;
     if(val > lean_2){
-      temp = 30;
+      Serial.print("Lean_2 ");
+      temp = 20;
     }
     else if(val < lean_max){
-      temp = 0;
+      temp = 10;
     }
     analogWrite(analog_out, temp);
   }
   else{
     Serial.print("Stioch ");   
-    analogWrite(analog_out, 95);
+    analogWrite(analog_out, 105);
   }
 }
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(9600);
   analogReference(DEFAULT);
   pinMode(analog_out, OUTPUT);
 }
