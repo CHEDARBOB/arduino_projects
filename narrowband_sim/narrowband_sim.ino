731ff32a44adde2fee768a0d0fa4c03d589f9470
Trevor <trevor.overby4@gmail.com>
2020-03-09 14:19:37 -0600

    narrowband slope 1st pass

--- narrowband_sim/narrowband_sim.ino
+++ narrowband_sim/narrowband_sim.ino
@@ -8,33 +8,27 @@ int max_narrowband_voltage = 1000; //mV
 */
 
 int analog_pin = A0;
-int analog_out = 6;
+int analog_out = 9;
 int ox_val = 0;
 int offset_ox_val = 0;
 float temp = 0;
 int o2_signal = 0;
+float aem_vol_num = 2.375;
+float aem_offset_num = 7.3125;
+float nar_voltage = 0;
 float magnitude = 0;
 //Wideband magic numbers
 int stoich = 637; //14.7
-float afr_range = 20;
+float afr_range = 1024;
 
-float set_magnitude(int afr){
- temp = stoich - afr;
- magnitude = (temp / afr_range);
- return abs(magnitude);
+
+float calc_afr(int voltage){
+ afr = (aem_vol_num * voltage) + aem_offset_num;
+ return afr;
 }
-void set_narrow_signal(int val){
-  //rich
-  if(val < stoich){
-    o2_signal = 160*set_magnitude(val);
-    if(o2_signal < 120){
-      o2_signal = 130;
-    }
-  }
-  //lean
-  else{
-    o2_signal = 40-(40*set_magnitude(val));
-  }
+void set_narrow_signal(int afr){
+  nar_voltage = (-0.77*afr) + 11.77;
+  o2_signal = nar_voltage * 255;
   Serial.print(" ");
   Serial.println(o2_signal);
   analogWrite(analog_out, o2_signal);
@@ -52,17 +46,7 @@ void loop() {
   //610 ~= 14.39 rich condition
   //637 ~= 14.7 stoich
   //665 ~= 15.03 lean condition
-  ox_val = analogRead(analog_pin);
+  analog_val = analogRead(analog_pin);
   Serial.print(ox_val);
-  if(ox_val < (stoich - afr_range)){
-    offset_ox_val = stoich - afr_range;
-    set_narrow_signal(offset_ox_val);
-  }
-  else if(ox_val > (stoich + afr_range)){
-    offset_ox_val = stoich + afr_range;
-    set_narrow_signal(offset_ox_val);
-  }
-  else{
-    set_narrow_signal(ox_val);
-  }
+  _vol = (analog_val / afr_range) * 5;
 }
