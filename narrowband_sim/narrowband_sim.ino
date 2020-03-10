62ccf7a84473eb28dda16ab12d9c14fad2ffd564
Trevor Overby <mr.blarg@gmail.com>
2020-03-10 14:12:44 -0600

    increased overall slope.

--- narrowband_sim/narrowband_sim.ino
+++ narrowband_sim/narrowband_sim.ino
@@ -1,36 +1,24 @@
-/*
-int adc_res = 1023;
-int lean_voltage = ; //mV
-int rich_voltage = ; //mV
-int max_duty_cycle = 255;
-int max_wideband_voltage = 5000; //mV
-int max_narrowband_voltage = 1000; //mV
-*/
-
 int analog_pin = A0;
-int analog_out = 9;
+int analog_out = 6;
 int analog_val = 0;
-int offset_ox_val = 0;
-float temp = 0;
+int count = 0;
 int o2_signal = 0;
 float _vol = 0.0;
 float afr = 0.0;
+//Wideband magic numbers
 float aem_vol_num = 2.375;
 float aem_offset_num = 7.3125;
+//
 float nar_voltage = 0.0;
-float magnitude = 0;
-//Wideband magic numbers
-int stoich = 637; //14.7
 float afr_range = 1024;
-
 
 float calc_afr(float voltage){
  afr = (aem_vol_num * voltage) + aem_offset_num;
- if(afr < 14.0){
-   afr = 14;
+ if(afr < 14.6){
+   afr = 14.6;
  }
- else if(afr > 15.2){
-  afr = 15.2;
+ else if(afr > 14.8){
+  afr = 14.8;
  }
  Serial.print(" ");
  Serial.print(afr);
@@ -38,15 +26,17 @@ float calc_afr(float voltage){
 }
 void set_narrow_signal(float _afr){
   Serial.print(" ");
-  nar_voltage = (-0.83*_afr) + 12.67;
+  nar_voltage = abs((-4.62*_afr) + 68.50);
+  Serial.print(nar_voltage);
+  Serial.print(" ");
   //Serial.println(nar_voltage);
-  o2_signal = 242*nar_voltage;
+  o2_signal = 255*nar_voltage;
   Serial.println(o2_signal);
   analogWrite(analog_out, o2_signal);
 }
 void setup() {
   //delay(20000);
-  Serial.begin(9600);
+  Serial.begin(115200);
   analogReference(DEFAULT);
   pinMode(analog_out, OUTPUT);
 }
@@ -57,7 +47,7 @@ void loop() {
   //610 ~= 14.39 rich condition
   //637 ~= 14.7 stoich
   //665 ~= 15.03 lean condition
-  analog_val = analogRead(analog_pin);
+  analog_val = analogRead(analog_pin); 
   _vol = (analog_val / afr_range) * 5;
   Serial.print(_vol);
   afr = calc_afr(_vol);
