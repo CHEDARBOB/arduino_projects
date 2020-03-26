5fe2c7cd5e579b7d2c932de4da25e7d6d2948a45
Trevor Overby <mr.blarg@gmail.com>
2020-03-26 14:29:59 -0600

    Biggie update. Most stable itteration.

--- narrowband_sim/narrowband_sim.ino
+++ narrowband_sim/narrowband_sim.ino
@@ -20,17 +20,18 @@ float calc_afr(float voltage){
  else if(afr > 15.20){
   afr = 15.20;
  }
- //Serial.print(" ");
- //Serial.print(afr);
+ Serial.print(" ");
+ Serial.print(afr);
  return afr;
 }
 void set_narrow_signal(float _afr){
-  Serial.print(" ");
+  //Serial.print(" ");
   //nar_voltage = abs((-4.62*_afr) + 68.50);
   //nar_voltage = abs((-2*_afr) + 30.4); //14.7 -- 15.2
   nar_voltage = abs((-1.0*_afr) + 15.2); //14.2 -- 15.2
-  //Serial.print(nar_voltage);
-  //Serial.print(" ");
+  Serial.print(nar_voltage);
+  Serial.print(" ");
+  //o2_signal = (180*nar_voltage)+20;
   o2_signal = 255*nar_voltage;
   Serial.println(o2_signal);
   analogWrite(analog_out, o2_signal);
@@ -50,9 +51,9 @@ void loop() {
   //665 ~= 15.03 lean condition
   analog_val = analogRead(analog_pin);
   Serial.print(analog_val);
-  //Serial.print(" "); 
+  Serial.print(" "); 
   _vol = (analog_val / afr_range) * 5;
-  //Serial.print(_vol);
+  Serial.print(_vol);
   afr = calc_afr(_vol);
   set_narrow_signal(afr);
 }
